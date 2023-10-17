a9945414aa3c5b5a2231767c62244e9445a18ab7
Pablo <pablonumon@gmail.com>
2023-10-17 19:07:52 +0200

    Cifrado simetrico 17/10 19:07

---
+++ CifradoSimetrioYAsimetrico/src/CifradoSimetrioYAsimetrico/Ejercicio1.java
@@ -0,0 +1,11 @@
+package CifradoSimetrioYAsimetrico;
+
+import java.util.Scanner;
+
+public class Ejercicio1 {
+
+	
+		
+	Scanner scanner = new Scanner (System.in);
+	String opcion ;
+}
---
+++ CifradoSimetrioYAsimetrico/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module CifradoSimetrioYAsimetrico {
+}
